2de858bdcccbda3ab4bdf0426f9cb48787c387b1
gonzalezben81 <gonzalezben393@gmail.com>
2025-01-02 21:31:01 +0000

    Updated and filtered data properly and updated server.R. The data is now sorted by District so it maps with the correct spatial polygon data.

--- server.R
+++ server.R
@@ -9,7 +9,7 @@ library(terra)
 library(sf)
 
 
-## Illinois Congressional Districts 
+## Illinois Congressional Districts
 # houseillower <- readOGR(dsn = "./data",layer = "tl_2016_17_sldl", verbose = FALSE)
 # houseillower <- sf::st_read("./data",layer = "tl_2016_17_sldl")
 # houseilupper <- readOGR(dsn = "./data",layer = "tl_2016_17_sldu", verbose = FALSE)
@@ -32,6 +32,7 @@ houseilupperframe <- as.data.frame(houseilupper)
 illinois_senate <- read.csv("./data/senate_info_2025.csv", stringsAsFactors = FALSE, encoding = "UTF-8")
 illinois_house <- read.csv("./data/house_info_2025.csv", stringsAsFactors = FALSE, encoding = "UTF-8")
 
+
 # Clean all character columns using iconv to remove invalid characters
 clean_columns <- function(df) {
   df[] <- lapply(df, function(x) {
@@ -51,58 +52,58 @@ illinois_house <- clean_columns(illinois_house)
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-  
+
   ##Rename variables to match data frame for coercion
   illinois_senate <- rename(illinois_senate, SLDUST = District)
-  
+
   ##Rename variables to match data frame for coercion
   illinois_house <- rename(illinois_house,SLDLST = District)
-  
+
   ##Can use cbind or left_join to bind the new data to the SpatialPolygonDataFrames
   houseilupper<- cbind(houseilupper, illinois_senate)
-  
+
   houseillower <- cbind(houseillower,illinois_house)
-  
-  
+
+
   ##Subsets the data to be used in a map and create grouping variables for overlays ####
   housedemocrats<- subset(houseillower, houseillower$Party %in% c("D"))
-  
+
   houserepublicans <- subset(houseillower, houseillower$Party %in% c("R"))
-  
+
   senatedemocrats<- subset(houseilupper, houseilupper$Party %in% c("D"))
-  
+
   senaterepublicans <- subset(houseilupper, houseilupper$Party %in% c("R"))
-  
-  ##Creates the popups for the datasets 
-  
-  senatedemo_popup <- paste0("<strong>Senator: </strong>", 
-                             senatedemocrats$Senator, 
-                             "<br><strong>Party: </strong>", 
+
+  ##Creates the popups for the datasets
+
+  senatedemo_popup <- paste0("<strong>Senator: </strong>",
+                             senatedemocrats$Senator,
+                             "<br><strong>Party: </strong>",
                              senatedemocrats$Party,
                              "<br><strong>District: </strong>",
                              senatedemocrats$SLDUST)
-  
-  senaterepub_popup <- paste0("<strong>Senator: </strong>", 
-                              senaterepublicans$Senator, 
-                              "<br><strong>Party: </strong>", 
+
+  senaterepub_popup <- paste0("<strong>Senator: </strong>",
+                              senaterepublicans$Senator,
+                              "<br><strong>Party: </strong>",
                               senaterepublicans$Party,
                               "<br><strong>District: </strong>",
                               senaterepublicans$SLDUST)
-  
-  housedemo_popup <- paste0("<strong>State Representative: </strong>", 
-                            housedemocrats$Representative, 
-                            "<br><strong>Party: </strong>", 
+
+  housedemo_popup <- paste0("<strong>State Representative: </strong>",
+                            housedemocrats$Representative,
+                            "<br><strong>Party: </strong>",
                             housedemocrats$Party,
                             "<br><strong>District: </strong>",
                             housedemocrats$SLDLST)
-  
-  houserepub_popup <- paste0("<strong>State Representative: </strong>", 
-                             houserepublicans$Representative, 
-                             "<br><strong>Party: </strong>", 
+
+  houserepub_popup <- paste0("<strong>State Representative: </strong>",
+                             houserepublicans$Representative,
+                             "<br><strong>Party: </strong>",
                              houserepublicans$Party,
                              "<br><strong>District: </strong>",
                              houserepublicans$SLDLST)
-  
+
   illinoismap<- leaflet(data = houseilupper) %>%
     addTiles() %>%
     addProviderTiles(
@@ -114,11 +115,11 @@ server <- function(input, output) {
     addPolygons(data = senaterepublicans,color = "red",popup = senaterepub_popup,group = "Senate Republican") %>%
     addPolygons(data = housedemocrats,color = "blue",popup = housedemo_popup,group = "House Democrat") %>%
     addPolygons(data = houserepublicans,color = "red",popup = houserepub_popup,group = "House Republican") %>%
-    # addPolygons(data = countyil,fillColor = ~pal3(TOT_POP), 
-    #             fillOpacity = 0.8, 
-    #             color = "#BDBDC3", 
-    #             weight = 1, 
-    #             popup = county_popup,group = "County") %>% 
+    # addPolygons(data = countyil,fillColor = ~pal3(TOT_POP),
+    #             fillOpacity = 0.8,
+    #             color = "#BDBDC3",
+    #             weight = 1,
+    #             popup = county_popup,group = "County") %>%
     addLayersControl(
       baseGroups = c(
         "OpenStreetMap"
@@ -127,10 +128,10 @@ server <- function(input, output) {
       options = layersControlOptions(collapsed = TRUE)
     ) %>%
     addMiniMap(toggleDisplay =  TRUE)%>%
-    addMeasure()%>% 
+    addMeasure()%>%
     addLegend("bottomleft",title = paste0("Illinois Congressional Map: Published ",format(Sys.time()," %b %Y")),colors = c("blue","red"),labels = c("D = Democrat","R = Republican"),values = ~TOT_POP)
   # addLegend("bottomright",values = ~TOT_POP,pal = pal6,title = "Population by County")
-  
+
   output$illinois <- renderLeaflet(illinoismap)
 }
 
